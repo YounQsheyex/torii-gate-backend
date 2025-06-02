966be08e00ce4e0f312ef61b1829f08414604dd5
YounQsheyex <younqsheyex@gmail.com>
2025-06-02 03:27:15 -0700

    added

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -13,7 +13,8 @@ const getLandlordsProperties = async (req, res) => {
       .sort("-createdAt")
       .skip(skip)
       .limit(limit);
-    const totalProperties = await PROPERTY.countDocuments({ landlord: userId });
+    const total = await PROPERTY.countDocuments({ landlord: userId });
+    const totalProperties = await PROPERTY.countDocuments(filter);
     const totalPages = Math.ceil(total / limit);
     const availableProperties = await PROPERTY.countDocuments({
       landlord: userId,
