c2221a8b61db92a75d7b896540bfa0522ef44d8c
YounQsheyex <younqsheyex@gmail.com>
2025-12-03 07:47:10 -0800

    ref:updated to send grid as render does not allow free gmail sender

--- emails/sendEmail.js
+++ emails/sendEmail.js
@@ -5,26 +5,49 @@ const {
   createResetTemplate,
 } = require("./emailTemplates");
 
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 const sendMail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
+  const msg = {
+    to,
+    from: process.env.EMAIL, // must be a verified sender in SendGrid
+    subject,
+    html,
+  };
+
   try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL,
-      to: to,
-      subject: subject,
-      html: html,
-    });
-    console.log(`email sent ${info.response}`);
+    const info = await sgMail.send(msg);
+    console.log(`Email sent successfully: ${info[0].statusCode}`);
   } catch (error) {
-    console.log(error);
+    console.error("Error sending email:", error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
   }
 };
+
+// const sendMail = async ({ to, subject, html }) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL,
+//       pass: process.env.PASSWORD,
+//     },
+//   });
+//   try {
+//     const info = await transporter.sendMail({
+//       from: process.env.EMAIL,
+//       to: to,
+//       subject: subject,
+//       html: html,
+//     });
+//     console.log(`email sent ${info.response}`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 const sendWelcomeEmail = ({ fullName, clientUrl, email }) => {
   const subject = "Welcome to Torii Gates";
